8c912d6443ae011db13a5f4ff1b1c0512965a636
Rajveer0916 <94645722+Rajveer0916@users.noreply.github.com>
2022-11-19 14:41:02 +0530

    Update list_of_commands.py

--- bot/helper/tg_helper/list_of_commands.py
+++ bot/helper/tg_helper/list_of_commands.py
@@ -3,9 +3,9 @@ from bot import CMD_INDEX
 class _BotCommands:
     def __init__(self) -> None:
         self.StartCommand = f'start{CMD_INDEX}'
-        self.MirrorCommand = f'mirror{CMD_INDEX}'
-        self.UnzipMirrorCommand = f'unzipmirror{CMD_INDEX}'
-        self.ZipMirrorCommand = f'zipmirror{CMD_INDEX}'
+        self.MirrorCommand = (f'mirror{CMD_INDEX}', f'm{CMD_INDEX}')
+        self.UnzipMirrorCommand = (f'unzipmirror{CMD_INDEX}', f'uzm{CMD_INDEX}')
+        self.ZipMirrorCommand = (f'zipmirror{CMD_INDEX}', f'zm{CMD_INDEX}')
         self.CancelMirror = f'cancel{CMD_INDEX}'
         self.CancelAllCommand = f'cancelall{CMD_INDEX}'
         self.ListCommand = f'list{CMD_INDEX}'
@@ -21,19 +21,19 @@ class _BotCommands:
         self.StatsCommand = f'stats{CMD_INDEX}'
         self.HelpCommand = f'help{CMD_INDEX}'
         self.LogCommand = f'log{CMD_INDEX}'
-        self.CloneCommand = f'clone{CMD_INDEX}'
+        self.CloneCommand = (f'clone{CMD_INDEX}' , f'c{CMD_INDEX}')
         self.CountCommand = f'count{CMD_INDEX}'
-        self.WatchCommand = f'watch{CMD_INDEX}'
-        self.ZipWatchCommand = f'zipwatch{CMD_INDEX}'
+        self.WatchCommand = (f'watch{CMD_INDEX}', f'w{CMD_INDEX}')
+        self.ZipWatchCommand = (f'zipwatch{CMD_INDEX}', f'zw{CMD_INDEX}')
         self.DeleteCommand = f'del{CMD_INDEX}'
         self.ShellCommand = f'shell{CMD_INDEX}'
         self.ExecHelpCommand = f'exechelp{CMD_INDEX}'
         self.LeechSetCommand = f'leechset{CMD_INDEX}'
         self.SetThumbCommand = f'setthumb{CMD_INDEX}'
-        self.LeechCommand = f'leech{CMD_INDEX}'
-        self.UnzipLeechCommand = f'unzipleech{CMD_INDEX}'
-        self.ZipLeechCommand = f'zipleech{CMD_INDEX}'
-        self.LeechWatchCommand = f'leechwatch{CMD_INDEX}'
-        self.LeechZipWatchCommand = f'leechzipwatch{CMD_INDEX}'
+        self.LeechCommand = (f'leech{CMD_INDEX}', f'l{CMD_INDEX}')
+        self.UnzipLeechCommand = (f'unzipleech{CMD_INDEX}', f'uzl{CMD_INDEX}')
+        self.ZipLeechCommand = (f'zipleech{CMD_INDEX}', f'zl{CMD_INDEX}')
+        self.LeechWatchCommand = (f'leechwatch{CMD_INDEX}', f'lw{CMD_INDEX}')
+        self.LeechZipWatchCommand = (f'leechzipwatch{CMD_INDEX}', f'lzw{CMD_INDEX}')
         
 BotCommands = _BotCommands()
